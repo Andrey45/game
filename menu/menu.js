a245dc3341c4758e2b3aac2a0026f1d3834a8478
Andrey <a.kobec@delaweb.ru>
2019-10-17 09:43:42 +0400

    final version menu

--- menu/menu.js
+++ menu/menu.js
@@ -1,11 +1,28 @@
 $(document).ready(function () {
+    let person = '';
+
+    $('#timon_card').click(function () {
+        person === '' || person === 'pumba'  ? person = 'timon' : '';
+        localStorage.setItem('person', person);
+        $('#timon_card').addClass('valid');
+        $('#pumba_card').removeClass('valid');
+    });
+
+    $('#pumba_card').click(function () {
+        person === '' || person === 'timon' ? person = 'pumba' : '';
+        localStorage.setItem('person', person);
+        $('#pumba_card').addClass('valid');
+        $('#timon_card').removeClass('valid');
+    });
+
     $('#but').click(function () {
         if(document.getElementById('name').value === ''){
             $('#name').addClass('error');
+        } else if(person === '') {
+            $('#invalid_person').text('Персонаж не выбран!!')
         } else {
             $('#name').removeClass('error');
-            // for database
-            //localStorage.setItem('name', name.value)
+            localStorage.setItem('name', name);
             location.href='../mult/index.html'
         }
     })
